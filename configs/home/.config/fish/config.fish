b335ab12bfdd1ceee7a7d8a1be38365e7185996c
Inmemorialake <inmemorialake2@gmail.com>
2026-09-16 09:36:13 -0500

    Config backup

--- configs/home/.config/fish/config.fish
+++ configs/home/.config/fish/config.fish
@@ -8,3 +8,11 @@ alias ..="cd .."
 
 
 fastfetch
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+set -gx MAMBA_EXE "/home/Inmemorialake/miniforge3/bin/mamba"
+set -gx MAMBA_ROOT_PREFIX "/home/Inmemorialake/miniforge3"
+set -gx JUPYTER_PATH /home/Inmemorialake/miniforge3/envs/cling/share/jupyter
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
